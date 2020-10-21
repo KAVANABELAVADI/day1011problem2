b42233f18039bcffcd9c87cd8a1075dec0bdb8aa
KAVANABELAVADI <50482786+KAVANABELAVADI@users.noreply.github.com>
2020-10-21 22:43:56 +0530

    till the difference is 2

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,22 +1,48 @@
 #!/bin/bash -x
 heads=0
 tails=0
+
+headstails ()
+{
+	if [ $((RANDOM%2)) == 1 ]
+        then
+		((heads++))
+                echo "heads Won"
+        else
+             ((tails++))
+	     echo "Tails Won"
+        fi
+}
+
+
 while (( $heads<21 && $tails<21 ))
 do
-	if [ $((RANDOM%2)) == 1 ]
-	then
-		echo "heads Won"
-		((heads++))
-	else
-		echo "Tails Won"
-		((tails++))
-	fi
+	headstails
 done
+
+
 
 if [ $heads == $tails ]
 then
    echo "Tie between Head and Tails"
+   flag=1
+	while [ $flag -eq 1 ]
+	do
+	  if [ $heads > $(($tails+2)) ]
+	  then
+		flag=0
+		 echo $heads $tails | awk '{print "Heads won the match by " (($1-$2))}'
+	  elif [ $tails > $(($heads+2)) ]
+	  then
+		flag=0
+  		echo $heads $tails | awk '{print "Tails won the match by " (($2-$1))}'
+	  else
+		headstails;
+	  fi
+	done
 fi
+
+
 
 if [ $heads -gt $tails ]
 then
